89de13d99c297e062506918b759148b9521d406e
teaheld <heldrich013@gmail.com>
2019-11-18 17:48:58 +0100

    Adding player with a click

--- sources/game.js
+++ sources/game.js
@@ -1,14 +1,15 @@
 class Game {
 	constructor() {
-		this.num_of_trials = 6;
+		this.max_number_of_trials = 6;
+		this.current_try = 0;
 	}
 	
 	write_trials() {
-		return this.num_of_trials;
+		return this.current_try;
 	}
 	
 	remove_trial() {
-		this.num_of_trials--;
+		this.current_try++;
 	}
 }
 
@@ -18,11 +19,46 @@ function newGame() {
 	g = new Game();
 	
 	document.getElementById("tryButton").style.visibility = "visible"; 
-	document.getElementById("pisi").innerHTML = g.write_trials();
 }
 
 function Try() {
 	g.remove_trial();
+}
+
+function addManu() {
+	add("../images/manu.webp");
+}
+
+function addLewi() {
+	add("../images/lewi.webp");
+}
+
+function addTomi() {
+	add("../images/tomi.webp");
+}
+
+function addKimich() {
+	add("../images/kimich.webp");
+}
+
+function addThiago() {
+	add("../images/thiago.webp");
+}
+
+function addPhil() {
+	add("../images/phil.webp");
+}
+
+function add(src) {
+	curr_try = g.current_try;
 	
-	document.getElementById("pisi").innerHTML = g.write_trials();
+	for(i = 1; i < 5; i++) {
+		img_i = "img_" + i.toString();
+		curr_src = document.getElementsByClassName(img_i)[curr_try].src.split("/").pop();
+		
+		if(curr_src === "logo.webp") {
+			document.getElementsByClassName(img_i)[curr_try].src = src;
+			break;
+		}
+	}
 }
